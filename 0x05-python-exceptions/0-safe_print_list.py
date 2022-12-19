58f7c5549b258b1acf2f0ecab829a89a1f7f05d6
ogbevireo <ogbevire@gmail.com>
2022-12-19 23:04:06 +0100

    fixed blank except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,11 +4,11 @@ def safe_print_list(my_list=[], x=0):
 
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end='')
-        except:
+            print("{}".format(my_list[i]), end="")
+        except TypeError:
             break
         else:
             count += 1
 
     print()
-    return (count)
+    return count
