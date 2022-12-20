dc9e45423d5ab598bf9142688a002d66edabdf0c
ogbevireo <ogbevire@gmail.com>
2022-12-20 22:55:26 +0100

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 class Square:
-    """Class Square that defines a square."""
+    """
+     Class Square that defines a square.
+
+    This class is for User
+    """
 
     pass
